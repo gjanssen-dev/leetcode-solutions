c40dd07091c38b130e54474cd28d5b78f43141d6
Gerrit Janssen <33416735+gjanssen-dev@users.noreply.github.com>
2025-03-25 17:43:37 +0900

    add: Two Sum

---
+++ 0001-two-sum/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const seen = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        const diff = target - num;
+
+        if (seen.has(diff)) {
+            return [seen.get(diff), i];
+        }
+
+        seen.set(num, i)
+    }
+};
