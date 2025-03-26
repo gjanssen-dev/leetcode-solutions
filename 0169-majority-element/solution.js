9a64275728ecf424d44e203f38c2d458d329d7be
Gerrit Janssen <33416735+gjanssen-dev@users.noreply.github.com>
2025-03-26 15:01:28 +0900

    add: Majority Element

---
+++ 0169-majority-element/solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    return majorityElemenBoyerMoore(nums);
+};
+
+function majorityElementNaive(num) {
+    // solution using hash map in O(n) time and space complexity
+    const freq = new Map();
+    for (const num of nums) {
+        freq.set(num, (freq.get(num) ?? 0) + 1);
+    }
+
+    return freq.entries().find(([, f]) => f > nums.length / 2)[0];
+};
+
+function majorityElemenBoyerMoore(nums) {
+    // solution using the Boyer-Moore majority vote algorithm
+    let candidate;
+    let count = 0;
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
